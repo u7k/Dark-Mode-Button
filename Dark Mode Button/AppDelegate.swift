1911701f24d1441f342ffcf0f155374b44cbaac0
Uygur Kıran <urkr@me.com>
2020-08-18 23:48:27 +0300

    apple events done.

--- Dark Mode Button/AppDelegate.swift
+++ Dark Mode Button/AppDelegate.swift
@@ -11,16 +11,44 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+     //props
+        var StatusBarItem: NSStatusItem? = nil
+        private let source = """
+    tell application "System Events"
+        tell appearance preferences
+            set dark mode to not dark mode
+        end tell
+    end tell
+    """
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        //dark mode button
+         StatusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+         
+        // StatusBarItem?.button?.image = NSImage(named: "")
+         StatusBarItem?.button?.title = "changeMode"
+         StatusBarItem?.button?.action = #selector(changeAppereance)
+        
+
+
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    
+    //MARK: - method
+       @objc func changeAppereance() {
+           if let script = NSAppleScript(source: source) {
+                      var error: NSDictionary?
+                      script.executeAndReturnError(&error)
+                      if let err = error {
+                       print(err)
+                      }
+                  }
+           
+       }
 
-}
+} //end
 

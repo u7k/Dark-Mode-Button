4de7adc1dd35a40c152d997ba6c6ef7c1adb681b
Uygur Kıran <urkr@me.com>
2020-08-18 23:38:44 +0300

    Initial Commit

---
+++ Dark Mode Button/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Dark Mode Button
+//
+//  Created by Uygur Kıran on 18.08.2020.
+//  Copyright © 2020 Uygur Kıran. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Dark Mode Button/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dark Mode Button
+//
+//  Created by Uygur Kıran on 18.08.2020.
+//  Copyright © 2020 Uygur Kıran. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

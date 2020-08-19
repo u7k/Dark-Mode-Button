85220b4df2d8a804c83e94eabf65c4ec33b732bb
Uygur Kıran <urkr@me.com>
2020-08-19 16:16:07 +0300

    v1.0

--- Dark Mode Button/AppDelegate.swift
+++ Dark Mode Button/AppDelegate.swift
@@ -8,47 +8,65 @@
 
 import Cocoa
 
-
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-     //props
-        var StatusBarItem: NSStatusItem? = nil
-        private let source = """
+    
+    //props
+    var StatusBarItem: NSStatusItem? = nil
+    let buttonMenu = NSMenu()
+    private let source = """
     tell application "System Events"
         tell appearance preferences
             set dark mode to not dark mode
         end tell
     end tell
     """
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         //dark mode button
-         StatusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
-         
-        // StatusBarItem?.button?.image = NSImage(named: "")
-         StatusBarItem?.button?.title = "changeMode"
-         StatusBarItem?.button?.action = #selector(changeAppereance)
+        StatusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        StatusBarItem?.button?.image = NSImage(named: "icon")
         
+        //button functionality
+        StatusBarItem?.button?.action = #selector(self.buttonClicked(sender:))
+        StatusBarItem?.button?.sendAction(on: [.leftMouseUp, .rightMouseUp])
         
+        //config menu
+        buttonMenu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: ""))
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
     
-    //MARK: - method
-       @objc func changeAppereance() {
-           if let script = NSAppleScript(source: source) {
-                      var error: NSDictionary?
-                      script.executeAndReturnError(&error)
-                      if let err = error {
-                       print(err)
-                      }
-                  }
-           
-       }
-
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+    }
+    
+    
+    //MARK: - methods
+    @objc func buttonClicked(sender: NSStatusBarButton) {
+        let event = NSApp.currentEvent!
+        
+        if event.type == NSEvent.EventType.rightMouseUp {
+            //right click
+            //add menu and perform the click
+            StatusBarItem?.menu = buttonMenu
+            StatusBarItem?.button?.performClick(nil)
+            //empty
+            StatusBarItem?.menu = nil
+        } else {
+            //normal click
+            changeAppereance()
+        }
+    }
+    
+    func changeAppereance() {
+        if let script = NSAppleScript(source: source) {
+            var error: NSDictionary?
+            script.executeAndReturnError(&error)
+            if let err = error {
+                print(err)
+            }
+        }
+        
+    }
+    
 } //end
 
